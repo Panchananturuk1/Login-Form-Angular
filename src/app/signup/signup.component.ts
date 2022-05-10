d4148594b236d290878771bf00b82d0ab75a10db
Panchananturuk1 <pturuk123@gmail.com>
2022-05-11 00:47:20 +0530

    7 files modified, 1 added

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent {
+  signup = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    contact: new FormControl('', [Validators.required]),
+  });
 
-  constructor() { }
-
-  ngOnInit() {
+  display: string = '';
+  getVal(val: string) {
+    console.log(val);
+    this.display = val;
   }
-
 }
